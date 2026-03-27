7318826d213fcef09d40f8ef78640b1263868198
filippotacconi <fillo.tacco@gmail.com>
2026-03-27 12:30:47 +0100

    mod utilities

--- ex2_utilities.py
+++ ex2_utilities.py
@@ -79,7 +79,7 @@ def bond_cash_flows(
     dates = [ref_date] + cash_flows_dates 
     cash_flows = pd.Series(
         data=[
-             (coupon_rate / coupon_freq) * notional
+             coupon_rate * year_frac_30e_360(dates[i-1],dates[i]) * notional
             for i in range(1, len(dates))  
         ],
         index=cash_flows_dates,
